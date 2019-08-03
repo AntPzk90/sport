a616705aa6ff67934ff1b174ac6dc8f5e06a3d37
AntPzk90 <44503472+AntPzk90@users.noreply.github.com>
2019-08-03 10:13:13 +0300

    add desktop version

--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,27 @@
 var bg = document.querySelector(".start-screen");
+var header = document.querySelector(".header");
 var aboutBlock = document.querySelector(".about-us");
 var advantages = document.querySelector(".advantages");
 var catalogItem = document.querySelector(".catalog-list__item");
 var catalogItemsAll = document.querySelectorAll(".catalog-list__item");
 var byuBtn = document.querySelector(".buy-btn");
 
-byuBtn.addEventListener("click",scrollTo(this,catalogItem,200))
 var overHendler = function(item){
      item.addEventListener("mouseover", function(evt){
           evt.preventDefault();
+          this.classList.add("catalog-list__item--shadow");
           var subCatalog = this.querySelector(".catalog-list__sub-list");
           subCatalog.classList.add("catalog-list__item--active");
      });
      item.addEventListener("mouseout", function(evt){
           evt.preventDefault();
+          this.classList.remove("catalog-list__item--shadow");
           var subCatalog = this.querySelector(".catalog-list__sub-list");
           subCatalog.classList.remove("catalog-list__item--active");
      });
      item.addEventListener("touchend",function(evt){
           evt.preventDefault();
+          this.classList.toggle("catalog-list__item--shadow");
           var subCatalog = this.querySelector(".catalog-list__sub-list");
           subCatalog.classList.toggle("catalog-list__item--active");
      });
@@ -27,17 +30,6 @@ for (var i = 0; i < catalogItemsAll.length; i++) {
      overHendler(catalogItemsAll[i]);
 }
 
-function scrollTo(element, to, duration) {
-     if (duration <= 0) return;
-     var difference = to - element.scrollTop;
-     var perTick = difference / duration * 10;
- 
-     setTimeout(function() {
-         element.scrollTop = element.scrollTop + perTick;
-         if (element.scrollTop === to) return;
-         scrollTo(element, to, duration - 10);
-     }, 10);
-}
 // var mobileBtn = document.querySelector(".header__mobile");
 // var nav = document.querySelector(".header__main-nav");
 // var userWrap = document.querySelector(".header__user-wrapper");
@@ -57,16 +49,20 @@ function scrollTo(element, to, duration) {
 // console.log(mobileBtn.classList.contains("header__mobile--close"));
 
 window.addEventListener("scroll", function(evt){
-    var scrolling = window.scrollY;
-    bg.style.backgroundPosition = "50% " + scrolling/5 + "px";
-    var h = advantages.getBoundingClientRect();
-    this.console.log(h.top)
-    if(h.top < 1000){
+     var scrolling = window.scrollY;
+     if(scrolling > 150){
+          header.classList.add("header--fixed");
+     }else{
+          header.classList.remove("header--fixed");
+     }
+     bg.style.backgroundPosition = "50% " + scrolling/5 + "px";
+     var h = advantages.getBoundingClientRect();
+     if(h.top < 1000){
          scrolling = Math.abs(h.top);
-         this.console.log(scrolling)
          advantages.style.backgroundPositionY = 100 + scrolling/4 + "px";
-    }
+     }
 });
+
 var scrollPos = 0;
 window.addEventListener("scroll", function(){
    var st = window.scrollY;
